c602136510afa60b7b0388c7aa14280575da50aa
Manish C S <cs.manish1603@gmail.com>
2026-05-13 16:43:29 +0530

    Connected frontend to deployed backend

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   const [searchId, setSearchId] = useState("");
 
-  const API_URL = "http://127.0.0.1:8000";
+  const API_URL = "https://fastapi-library-backend.onrender.com";
 
   // -------------------------------------
   // GET ALL BOOKS
